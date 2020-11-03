46209ce9b52060f7c896978efc3f6aa950ea0d74
hanih <hamadeh.hani@gmail.com>
2020-11-03 12:25:39 +0200

    Uncommented reading file

--- plot4.R
+++ plot4.R
@@ -1,9 +1,9 @@
-#library(dplyr)
-#household_power_consumption <- read.table("household_power_consumption.txt", header=T, sep=";", na.strings = "?",
-#                                         colClasses = c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"))
-#consumption <- as_tibble(household_power_consumption) %>%
-#  mutate(datetime = as.POSIXct(strptime(paste(Date, Time), tz="", format = "%d/%m/%Y %H:%M"))) %>%
-#  filter(datetime >= as.POSIXct("2007-02-01") & datetime < as.POSIXct("2007-02-03")) 
+library(dplyr)
+household_power_consumption <- read.table("household_power_consumption.txt", header=T, sep=";", na.strings = "?",
+                                         colClasses = c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"))
+consumption <- as_tibble(household_power_consumption) %>%
+  mutate(datetime = as.POSIXct(strptime(paste(Date, Time), tz="", format = "%d/%m/%Y %H:%M"))) %>%
+  filter(datetime >= as.POSIXct("2007-02-01") & datetime < as.POSIXct("2007-02-03")) 
 
 png("plot4.png")
 par(mfcol=c(2,2))
